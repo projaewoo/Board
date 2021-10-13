8c481bd852cc50aa6448756fc70e8792b38c718e
jeongjw <jw.jeong@sqisoft.com>
2021-10-13 19:22:39 +0900

    초기 커밋

---
+++ src/components/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainPage = () => {
+  return(
+    <MainPageWrapper>
+      mainPage
+    </MainPageWrapper>
+  );
+};
+
+const MainPageWrapper = styled.div`
+
+`;
+
+export default MainPage;
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavBar = () => {
+  return <NavBarWrapper>NavBar</NavBarWrapper>;
+};
+
+const NavBarWrapper = styled.div``;
+
+export default NavBar;
